dc985b2a0164a3b5c95a3b1dea70161f338d6c53
Felipe Falco <falco.felipe27@gmail.com>
2023-03-09 10:21:15 +1300

    Initial Commit

---
+++ LRUCache.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hapara_Cache_Exercise
+{
+    internal class LRUCache
+    {
+        private int capacity;
+        private Dictionary<int, int> activeCache;
+        private List<int> lastUsedKeys;
+
+        public LRUCache(int _capacity)
+        {
+            if (_capacity <= 0)
+                throw new Exception("Please ensure that the value passed on to LRUCache constructor is > 0.");
+
+            capacity = _capacity;
+            lastUsedKeys = new(_capacity);
+            activeCache = new(_capacity);
+
+            Console.WriteLine("New LRUCache being created with capacity {0}", capacity);
+        }
+
+        public void put(int key, int value)
+        {
+            Console.WriteLine("PUT: key {0} value {1}", key, value);
+            int lastKeyRemoved = AddToRecentKeys(key);
+
+            if (!activeCache.ContainsKey(key) && activeCache.Count >= capacity)
+            {
+                activeCache.Remove(lastKeyRemoved);
+                Console.WriteLine("Removed key {0} from dictionary", lastKeyRemoved);
+            }
+
+            activeCache[key] = value;
+            Console.WriteLine("Added or Updated key {0} with value {1} from dictionary", key, value);
+            return;
+        }
+
+        public int get(int key) 
+        {
+            Console.WriteLine("GET: key {0}", key);
+            if (!activeCache.ContainsKey(key))
+                return -1;
+
+            AddToRecentKeys(key);
+
+            return activeCache[key];
+        }
+        public int delete(int key)
+        {
+            Console.WriteLine("DELETE: key {0}", key);
+            if (!activeCache.ContainsKey(key))
+                return -1;
+
+            int deletedValue = activeCache[key];
+            activeCache.Remove(key);
+            DeleteFromRecentKeys(key);
+
+            return deletedValue;
+        }
+
+        private int AddToRecentKeys(int key)
+        {
+            
+            int keyIndex = lastUsedKeys.FindIndex(k => k == key);
+            if (keyIndex > -1)
+            {
+                lastUsedKeys.RemoveAt(keyIndex);
+            }
+
+            int lastKeyRemoved = TrimList(lastUsedKeys, capacity - 1);
+
+            lastUsedKeys.Add(key);
+
+            return lastKeyRemoved;
+        }
+        private void DeleteFromRecentKeys(int key)
+        {
+            lastUsedKeys.Remove(key);
+        }
+
+        private int TrimList(List<int> list, int capacity)
+        {
+            int lastKeyRemoved = -1;
+            while (lastUsedKeys.Count > capacity)
+            {
+                lastKeyRemoved = lastUsedKeys[0];
+                lastUsedKeys.RemoveAt(0);
+            }
+
+            return lastKeyRemoved;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Hapara_Cache_Exercise;
+
+LRUCache cache = new LRUCache(2 /* capacity */);
+
+cache.put(1, 1);
+cache.put(2, 2);
+Console.WriteLine("Get 1: {0}", cache.get(1));     // returns 1
+cache.put(3, 3);    // evicts key 2
+Console.WriteLine("Get 2: {0}", cache.get(2));     // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+Console.WriteLine("Get 1: {0}", cache.get(1));       // returns -1 (not found)
+Console.WriteLine("Get 3: {0}", cache.get(3));        // returns 3
+Console.WriteLine("Get 4: {0}", cache.get(4));        // returns 4
+Console.WriteLine("Delete 3: {0}", cache.delete(3));    // returns 3
+Console.WriteLine("Get 3: {0}", cache.get(3));        // returns -1 (not found)
+
+/* Extra Tests */
+/*
+cache.put(1, 1);
+cache.put(2, 4);
+cache.put(3, 6);
+Console.WriteLine("Get 2: {0}", cache.get(2));
+Console.WriteLine("Get 1: {0}", cache.get(1));
+cache.put(2, 3);
+cache.put(4, 7);
+Console.WriteLine("Get 2: {0}", cache.get(2));
+Console.WriteLine("Get 3: {0}", cache.get(3));
+Console.WriteLine("Get 4: {0}", cache.get(4));
+Console.WriteLine("Delete 2: {0}", cache.delete(2));
+cache.put(5, 1);
+cache.put(6, 2);
+Console.WriteLine("Get 5: {0}", cache.get(5));
+Console.WriteLine("Get 4: {0}", cache.get(4));
+*/
